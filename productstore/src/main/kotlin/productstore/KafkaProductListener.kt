cc5cfd2f985d445fcfc02c9f001f72ea7752dc8b
Alex <saker.alexandre@gmail.com>
2018-10-19 23:31:44 +0200

    added multi modules support

--- productstore/src/main/kotlin/productstore/KafkaProductListener.kt
+++ productstore/src/main/kotlin/productstore/KafkaProductListener.kt
@@ -3,7 +3,7 @@ package productstore
 import io.micronaut.configuration.kafka.annotation.*
 
 @KafkaListener(offsetReset = OffsetReset.LATEST)
-class KafkaProductListener {
+class KafkaProductListener{
 
     @Topic("my-products")
     fun receive(@KafkaKey brand: String, product: Product) {
