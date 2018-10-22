efae72ed3a1d04b49c6d3802f2cdc4502a1ad24e
Alexandre SAKER <asaker@ncelrnd2369.nce.amadeus.net>
2018-10-22 07:51:40 +0200

    Added redis connection to product store

--- productstore/src/main/kotlin/productstore/KafkaProductListener.kt
+++ productstore/src/main/kotlin/productstore/KafkaProductListener.kt
@@ -1,12 +1,28 @@
 package productstore
-
+import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.reactive.RedisReactiveCommands
 import io.micronaut.configuration.kafka.annotation.*
+import javax.inject.Inject
+import org.slf4j.LoggerFactory
 
 @KafkaListener(offsetReset = OffsetReset.LATEST)
-class KafkaProductListener {
+class KafkaProductListener @Inject constructor(
+        private val redisConnection: StatefulRedisConnection<String, String>,
+){
+    private lateinit var redisReactiveCmds: RedisReactiveCommands<String, String>
+    private val logger = LoggerFactory.getLogger(KafkaProductListener::class.java)
 
+    init {
+        try {
+            redisReactiveCmds = redisConnection.reactive()
+        } catch (ex: RuntimeException) {
+            logger.error(ex.message)
+        }
+
+    }
     @Topic("my-products")
     fun receive(@KafkaKey brand: String, product: Product) {
         println("Got Product - ${product.name} by $brand @ ${product.price} (Stock : ${product.quantity})")
+        redisReactiveCmds.set(product.name,product.quantity.toString()).block()
     }
 }
